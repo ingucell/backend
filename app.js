8f28aad2c886351fc10b2fe3c37e667212ce1777
ingucell <mathiaslawson70@gmail.com>
2022-11-24 11:20:40 +0000

    bcrypt installation, express server setup

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express();
+const PORT  = process.env.PORT || 5000
+
+
+
+
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+
+
+
+
+
